0220864c934dbd9c52812d347b9a44fbc15a276c
Rahul Deb <115576849+2onedev@users.noreply.github.com>
2024-08-24 20:55:19 +0530

    Update fibonacci.java
    Just a way using which you can give the first number and second number as input,, nothing big but kinda cool

--- fibonacci.java
+++ fibonacci.java
@@ -1,12 +1,13 @@
-
 import java.util.Scanner;
 public class fibonacci {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
         System.out.print("Enter the number of terms: ");
         int terms = n.nextInt();
-        int first = 2;
-        int second = 4;
+        System.out.print("Enter the first number: ");
+        int first = n.nextInt();
+        System.out.print("Enter the second number: ");
+        int second = n.nextInt();
         System.out.println("the number of terms are:" + terms);
         System.out.println("the first term is: " + first);
         for (int i = 1; i <= terms; i++) {
