bf6a48176b0024f8e88ecd6ed3bd74eb84e10695
Rahul Deb <115576849+2onedev@users.noreply.github.com>
2024-09-07 00:54:18 +0530

    SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+     
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            Scanner n = new Scanner(System.in);
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number>0 || number<0) {
+                sum = sum + number;
+                
+            }else if(number==0){
+                break;
+            }else{
+                System.out.println(" ");
+            }
+            
+        }
+        System.out.println("Sum of the numbers:  " + sum);
+
+    }
+}
